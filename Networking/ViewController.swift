a9a144205dfff47c514f21bd7cddebfcf9be5ab2
Saiteja Alle <alle.saiteja@gmail.com>
2017-07-01 22:39:29 -0400

    Updated the structure of App

--- Networking/ViewController.swift
+++ Networking/ViewController.swift
@@ -20,12 +20,22 @@ class ViewController: UIViewController {
     
     var firebase:BaseFire!
     
+    var userDefaults:UserDefaults!
+    
 //    var posts:[Posts] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         firebase = BaseFire()
+        
+        
+        if userDefaults != nil {
+            print("userDefaults has a value")
+           self.performSegue(withIdentifier: "successFul", sender: self)
+        }
+        
+        
+        
         
 //        let databaseRef = FIRDatabase.database().reference().child("posts")
 //        
@@ -52,6 +62,10 @@ class ViewController: UIViewController {
         firebase.loginUser(userName: userNameTF.text!, password: passswordTF.text!) {[unowned self](user, error) in
             if error == nil {
                 self.performSegue(withIdentifier: "successFul", sender: self)
+                self.userDefaults = UserDefaults.standard
+                self.userDefaults.set(self.userNameTF.text!, forKey: "Username")
+                self.userDefaults.set(self.passswordTF.text, forKey: "Password")
+
             }
             else {
                 print("Something is Wrong")
